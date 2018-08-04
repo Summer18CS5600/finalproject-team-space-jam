34b9e9845be3a649a1fecdccce24a26ba92ca960
Arnold Esguerra <esguerra.ar@husky.neu.edu>
2018-08-04 12:23:22 -0400

    update cachelines and hidden fields

--- model/board/board.model.server.js
+++ model/board/board.model.server.js
@@ -5,25 +5,24 @@ var BoardModel = mongoose.model("BoardModel", BoardSchema);
 //are bound to the functions below. this is essentially defining the api.
 BoardModel.createBoard = createBoard;
 BoardModel.findBoard = findBoard;
-BoardModel.accessMemory = accessMemory;
+BoardModel.updateBoard = updateBoard;
 
 module.exports = BoardModel;
 
 //call the db specific functions.
 function createBoard(board) {
-  console.log("in model");
+  console.log("MODEL: creating board: " + board.boardId);
   console.log(board);
   var newBoard = board;
   return BoardModel.create(newBoard);
 }
 
 function findBoard(boardId) {
-  console.log("in model finding board: " + boardId);
+  console.log("MODEL: finding board: " + boardId);
   return BoardModel.findOne({boardId: boardId});
 }
 
-function accessMemory(boardId, value) {
-  console.log("in model access memory" + boardId + " : " + value);
-
-  return BoardModel.findOne({boardId: boardId});
+function updateBoard(boardId, board) {
+  console.log("MODEL: updating board: " + boardId);
+  return BoardModel.update({boardId: boardId}, board);
 }
