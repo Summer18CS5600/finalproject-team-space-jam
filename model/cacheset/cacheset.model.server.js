03b84145d0b1d76fc3b69fb7c96733addab1fd98
Michael Hanna <mhanna14151@gmail.com>
2018-08-04 14:14:22 -0400

    Saving the cache set starter code.

---
+++ model/cacheset/cacheset.model.server.js
@@ -0,0 +1,28 @@
+var mongoose = require('mongoose');
+var CacheSetSchema = require("./cacheset.schema.server");
+var CacheSetModel = mongoose.model("CacheSetModel", CacheSetSchema);
+
+//are bound to the functions below. this is essentially defining the api.
+CacheSetModel.createCacheSet = createCacheSet;
+CacheSetModel.findCacheSet = findCacheSet;
+CacheSetModel.updateCacheSet = updateCacheSet;
+
+module.exports = CacheSetModel;
+
+//call the db specific functions.
+function createCacheSet(cacheSet) {
+  console.log("MODEL: creating cacheSet: " + cacheSet.cacheSetId);
+  console.log(cacheSet);
+  var newCacheSet = cacheSet;
+  return CacheSetModel.create(newCacheSet);
+}
+
+function findCacheSet(cacheSetId) {
+  console.log("MODEL: finding cacheSet: " + cacheSetId);
+  return CacheSetModel.findOne({cacheSetId: cacheSetId});
+}
+
+function updateCacheSet(cacheSetId, cacheSet) {
+  console.log("MODEL: updating cacheSet: " + cacheSetId);
+  return CacheSetModel.update({cacheSetId: cacheSetId}, cacheSet);
+}
