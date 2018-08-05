72c3130e0f05322a83462c5708b0381e23b7dfd7
Michael Hanna <mhanna14151@gmail.com>
2018-08-05 13:01:51 -0400

    made necessary revisions to updateCacheSet.

--- server/services/board.service.server.js
+++ server/services/board.service.server.js
@@ -98,52 +98,53 @@ module.exports = function(app){
     // then just make this a global variable. Maybe best to pass the evict policy type from the board Service.
     // Fist grab the current cacheSet from the Model
     cacheSetModel.findCacheSet(boardId).then(function(cacheSet) {
+      console.log("in server... before");
       console.log(cacheSet);
+      console.log("in server after:");
       cacheSet['totalOccurrences'] = cacheSet['totalOccurrences'] + 1;
       // Second, do the update logic on the cacheSet (cacheHit, cacheMiss, if cacheMiss, evict or cold miss?)
-      for (var i = 0; i < cacheSet.length; i++) {
-        console.log("The lineId is: " + cacheSet[i]['lineId']);
-        console.log("The tiles are is: " + cacheSet[i]['tiles']);
-        console.log("The age is: " + cacheSet[i]['age']);
+      for (var i = 0; i < cacheSet['setOfCacheLines'].length; i++) {
+        console.log("The lineId is: " + cacheSet['setOfCacheLines'][i]['lineId']);
+        console.log("The tiles are is: " + cacheSet['setOfCacheLines'][i]['tiles']);
+        console.log("The age is: " + cacheSet['setOfCacheLines'][i]['age']);
       }
       var cacheLine = tiles[0]['cacheLine'];
       var didWeMiss = true;
       // Find out if there's been a cacheHit
-      for (var a = 0; i < cacheSet.length; a++) {
-        if (cacheSet[a]['lineId'] == cacheLine) {
+      for (var a = 0; i < cacheSet['setOfCacheLines'].length; a++) {
+        if (cacheSet['setOfCacheLines'][a]['lineId'] == cacheLine) {
           // the cacheHasBeenHit
-          cacheSet[a]['age'] = cacheSet[totalOccurrences];
+          cacheSet['setOfCacheLines'][a]['age'] = cacheSet[totalOccurrences];
           didWeMiss = false;
         }
       }
       if (didWeMiss) {
         // we didn't hit the cache.
         // First Check is there open room in the cache. If so, it's a cold miss.
-        if (cacheSet.length < 4) {
-          cacheSet[cacheSet.length] = {lineId: cacheLine, tiles: tiles, age: cacheSet['totalOccurrences']};
+        var firstEmptyPosition = cacheSet['setOfCacheLines'].length;
+        if (firstEmptyPosition < 4) {
+          cacheSet['setOfCacheLines'].push({lineId: cacheLine, tiles: tiles, age: cacheSet['totalOccurrences']});
         } else{
           // there's no more room in the cache, let's check for the LRU of the group, later we can make a flag for the
           if (cachePolicyIsLRU) {
             var currentAge = 100000000000000000000;
             var tracker = -1;
             // find the oldest line (the one to replace).
-            for (var t = 0; t <cacheSet.length; t++) {
-              if (cacheSet[t]['age'] < currentAge) {
-                currentAge = cacheSet[t]['age'];
-                oldestLine = cacheSet[t]['lineId'];
+            for (var t = 0; t <cacheSet['setOfCacheLines'].length; t++) {
+              if (cacheSet['setOfCacheLines'][t]['age'] < currentAge) {
+                currentAge = cacheSet['setOfCacheLines'][t]['age'];
+                oldestLine = cacheSet['setOfCacheLines'][t]['lineId'];
                 tracker = t;
               }
             }
             // now update the cacheSet by replacing the oldestLine
-            cacheSet[tracker] = {lineId: cacheLine, tiles: tiles, age: cacheSet['totalOccurrences']};
+            cacheSet['setOfCacheLines'][tracker] = {lineId: cacheLine, tiles: tiles, age: cacheSet['totalOccurrences']};
             // send boardService back the oldestLine for eviction
           }
         }
       }
-
       // Third, push the updates to the model
       cacheSetModel.updateCacheSet(boardId, cacheSet);
-
       if (oldestLine > -1) {
         hideCacheLine(boardId, oldestLine);
       }
